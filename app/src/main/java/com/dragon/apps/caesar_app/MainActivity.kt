bcf483e0f780ade4e953b0e0286f22681d264b07
Muhammad-Saeed-0 <muhammad01111417159@gmail.com>
2023-02-23 11:17:26 +0200

    test

--- app/src/main/java/com/dragon/apps/caesar_app/MainActivity.kt
+++ app/src/main/java/com/dragon/apps/caesar_app/MainActivity.kt
@@ -2,10 +2,44 @@ package com.dragon.apps.caesar_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val encr :Button = findViewById(R.id.encr_btn)
+        val decr :Button = findViewById(R.id.decr_btn)
+
+        encr.setOnClickListener{
+            val ciper = Cipher()
+            ciper.encryption("he!l o#", 4)
+        }
+
+        decr.setOnClickListener{
+
+        }
     }
+}
+
+class Cipher
+{
+    private val alpha = "abcdefghijklmnopqrstuvwxyz !@#$"
+    private var alphaLen = alpha.length
+    fun  encryption(plainText: String, k: Int = 4)
+    {
+        val TAG = "encryption"
+
+        var plainLen = plainText.length
+        var cipher = ""
+        for (item in plainText)
+        {
+            cipher +=  alpha[ (k + alpha.indexOf(item)) % alphaLen ]
+        }
+
+        Log.d(TAG, "encryption: " + cipher)
+    }
+
 }
