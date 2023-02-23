70dba604196acb824cb77e72b964c3a04695c79a
Muhammad-Saeed-0 <muhammad01111417159@gmail.com>
2023-02-23 11:52:56 +0200

    encryptionEnd

--- app/src/main/java/com/dragon/apps/caesar_app/MainActivity.kt
+++ app/src/main/java/com/dragon/apps/caesar_app/MainActivity.kt
@@ -4,21 +4,38 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val encr :Button = findViewById(R.id.encr_btn)
-        val decr :Button = findViewById(R.id.decr_btn)
+        // get the encryption button
+        val encryption :Button = findViewById(R.id.encr_btn)
+        // get the decryption button
+        val decryption :Button = findViewById(R.id.decr_btn)
+        // get the entered text - text input
+        val enteredText : TextInputEditText= findViewById(R.id.entered_text)
+        // get the result - text input
+        val res : TextInputEditText= findViewById(R.id.res)
 
-        encr.setOnClickListener{
-            val ciper = Cipher()
-            ciper.encryption("he!l o#", 4)
+        // create an object from cipher class
+        val cipher = Cipher()
+
+        // encryption button listener
+        encryption.setOnClickListener{
+            // get the entered text
+            val text =  enteredText.text.toString()
+            // get the text after encryption
+            val cipherText = cipher.encryption(text, 4)
+            // display encryption text
+            res.setText(cipherText)
         }
 
-        decr.setOnClickListener{
+        // decryption button listener
+        decryption.setOnClickListener{
 
         }
     }
@@ -28,18 +45,16 @@ class Cipher
 {
     private val alpha = "abcdefghijklmnopqrstuvwxyz !@#$"
     private var alphaLen = alpha.length
-    fun  encryption(plainText: String, k: Int = 4)
-    {
-        val TAG = "encryption"
 
+    fun  encryption(plainText: String, k: Int = 4) : String
+    {
         var plainLen = plainText.length
         var cipher = ""
+
         for (item in plainText)
         {
             cipher +=  alpha[ (k + alpha.indexOf(item)) % alphaLen ]
         }
-
-        Log.d(TAG, "encryption: " + cipher)
+        return cipher
     }
-
 }
